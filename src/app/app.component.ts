d17e78faddc767465050578875336cc216494d12
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-26 10:30:59 +0530

    Update and Delete complete, CRUD Finish.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,31 +30,67 @@ export class AppComponent implements OnInit {
   }
 
   closeModel() {
+    this.studentObj = new Student();
     const myModel = document.getElementById('createStudentModel');
     if (myModel != null) {
       myModel.style.display = 'none';
     }
   }
 
-  addStudent() {
-    const myModel = document.getElementById('createStudentModel');
-    if (myModel != null) {
-      myModel.style.display = 'none';
+  onEdit(student: Student) {
+    this.studentObj = student;
+    this.openStudentModel();
+  }
+  onDelete(student: Student) {
+    // Show confirmation dialog
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete ${student.name}?`
+    );
+
+    if (confirmDelete) {
+      // If the user confirms, proceed with deletion
+      this.studentList = this.studentList.filter((m) => m.id != student.id);
+      localStorage.setItem('angular17-crud', JSON.stringify(this.studentList));
     }
+  }
+
+  addStudent() {
     const isLocalStorage = localStorage.getItem('angular17-crud');
     if (isLocalStorage != null) {
       const oldArr = JSON.parse(isLocalStorage);
+      this.studentObj.id = oldArr.length + 1;
       oldArr.push(this.studentObj);
+      this.studentList = oldArr;
       localStorage.setItem('angular17-crud', JSON.stringify(oldArr));
     } else {
       const newArr = [];
       newArr.push(this.studentObj);
+      this.studentObj.id = 1;
+      this.studentList = newArr;
       localStorage.setItem('angular17-crud', JSON.stringify(newArr));
+    }
+    this.closeModel();
+  }
+
+  updateStudent() {
+    const currentRecord = this.studentList.find(
+      (m) => (m.id = this.studentObj.id)
+    );
+    if (currentRecord != undefined) {
+      currentRecord.name = this.studentObj.name;
+      currentRecord.mobileNo = this.studentObj.mobileNo;
+      currentRecord.email = this.studentObj.email;
+      currentRecord.city = this.studentObj.city;
+      currentRecord.address = this.studentObj.address;
+      currentRecord.pincode = this.studentObj.pincode;
+      localStorage.setItem('angular17-crud', JSON.stringify(this.studentList));
+      this.closeModel();
     }
   }
 }
 
 export class Student {
+  id: number;
   name: string;
   mobileNo: string;
   email: string;
@@ -63,6 +99,7 @@ export class Student {
   pincode: string;
 
   constructor() {
+    this.id = 0;
     this.name = '';
     this.mobileNo = '';
     this.email = '';
